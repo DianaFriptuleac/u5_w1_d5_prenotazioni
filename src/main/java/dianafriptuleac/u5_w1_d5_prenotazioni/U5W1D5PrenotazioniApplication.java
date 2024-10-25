51de55d58ac2f73e47df486426cc8b0befcb7c83
Diana <dianadorojuc3@gmail.com>
2024-10-25 10:42:32 +0200

    Initial commit

---
+++ src/main/java/dianafriptuleac/u5_w1_d5_prenotazioni/U5W1D5PrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5_w1_d5_prenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D5PrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D5PrenotazioniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianafriptuleac/u5_w1_d5_prenotazioni/U5W1D5PrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5_w1_d5_prenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D5PrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
